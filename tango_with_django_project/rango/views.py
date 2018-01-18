9b17e72ee92653a8d972b37b11182f917cfa7779
Ajnos <2271916k@student.gla.ac.uk>
2018-01-18 09:56:32 +0000

    Chapter 2 Exercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner")
+    return HttpResponse("Welcome Page <a href='/rango/about/'>Go to: About</a>")
+def about(request):
+    return HttpResponse("Rango says here is the about page <a href='/rango/'>Go to: Main</a>")
 
 # Create your views here.
